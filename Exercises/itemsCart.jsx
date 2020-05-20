b207daca75bcfd9c77277dcc2898cc48e97688c2
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-05-20 14:36:33 +0530

    Update itemsCart.jsx

--- Exercises/itemsCart.jsx
+++ Exercises/itemsCart.jsx
@@ -1,56 +1,75 @@
 import React, { Component } from 'react';
+import { getItems } from "./itemList";
 
 class CartItems extends Component { 
     state = {
-        count: 0,
-    }
-    
-    listItems(){
-        return (
-            <div>
-                <span className={this.getBadge()}>{this.setCount()}</span>
-                <button onClick={this.handleAdd} className='btn m-4 btn-secondary'>Add</button>
-                <button onClick={this.handleRemove} className='btn m-4 btn-secondary'>Remove</button>
-                <button className='btn m-4 btn-danger'>Delete</button>
-            </div>
-        );
-    }
+        total: 0,
+        items: getItems(),
+    };
     
     handleAdd = () => {
-        this.setState({count:this.state.count + 1})
     }
     
     handleRemove = () => {
-        this.setState({count:this.state.count - 1})
     }
     
+    handleDelete = (item) => {
+    // const items = this.state.items.filter((i) => i.id !== item.id);
+    // this.setState({ items });
+    };
+    
     render() { 
-        return (
-            <div>
-                <div>
-                    <p>No. of Items: {this.setCount()}</p>
-                    <button onClick={this.resetCount} className='btn m-2 btn-primary'>Reset</button>
-                    {this.listItems()}
-                </div>
-            </div>
-        );
+        const { length: count } = this.state.items;
+
+    if (count === 0) return <p>There are no items in the Cart</p>;
+
+    return (
+      <React.Fragment>
+        <p>No. of items in the cart are {this.setCount()}</p>
+        <span onClick={this.resetCount} className="btn m-2 btn-primary">
+          Reset
+        </span>
+        <table>
+          <tbody>
+            {this.state.items.map((item) => (
+              <tr key={item.id}>
+                <h5>
+                  <span className="badge m-2">
+                    Product:{item.product},InStock:{item.instock}
+                    <p className={this.getBadge()}>{item.count}</p>
+                    <button onClick={this.handleAdd} className="btn m-4 btn-secondary">
+                      Add
+                    </button>
+                    <button onClick={this.handleRemove} className="btn m-4 btn-secondary">
+                      Remove
+                    </button>
+                    <button onClick={this.handleDelete(item)} className="btn m-4 btn-danger">
+                      Delete
+                    </button>
+                  </span>
+                </h5>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
     }
     
     setCount(){
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
+        const {total} = this.state;
+        return total === 0 ? 'Zero' : total;
     }
     
     resetCount = () =>{
-        let {count} = this.state;
-        this.setState({count: count = 0});
+        let {total} = this.state;
+        this.setState({total: total = 0});
     }
     
     getBadge(){
         let classes = 'badge m-2 badge-';
-        return classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes += this.state.total === 0 ? 'warning' : 'primary';
     }
 }
-
 
 export default CartItems;
