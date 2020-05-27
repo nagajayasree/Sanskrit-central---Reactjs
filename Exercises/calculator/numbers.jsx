a993a43205adb52c5c8439897ecfd38ae01a5daf
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-05-27 22:41:51 +0530

    Update numbers.jsx

--- Exercises/calculator/numbers.jsx
+++ Exercises/calculator/numbers.jsx
@@ -1,20 +1,27 @@
 const numbers = [
-  { id: "op", value: "clear" },
   { id: "0", value: 0 },
-  { id: "div", value: "/" },
-  { id: "7", value: 7 },
-  { id: "8", value: 8 },
-  { id: "9", value: 9 },
-  { id: "sub", value: "-" },
-  { id: "4", value: 4 },
-  { id: "5", value: 5 },
-  { id: "6", value: 6 },
-  { id: "add", value: "+" },
   { id: "1", value: 1 },
   { id: "2", value: 2 },
   { id: "3", value: 3 },
+  { id: "4", value: 4 },
+  { id: "5", value: 5 },
+  { id: "6", value: 6 },
+  { id: "7", value: 7 },
+  { id: "8", value: 8 },
+  { id: "9", value: 9 },
+];
+
+const operators = [
+  { id: "add", value: "+" },
+  { id: "sub", value: "-" },
+  { id: "mul", value: "*" },
+  { id: "div", value: "/" },
   { id: "result", value: "=" },
 ];
+
+export const getOperators = () => {
+  return operators;
+};
 
 const getNumbers = () => {
   return numbers;
